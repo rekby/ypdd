ce86820095f3b227c7a8ae6b9368b5cb59eca9d4
Rekby <timofey@koolin.ru>
2016-11-04 09:02:22 +0300

    install nslookup

--- travis-test.sh
+++ travis-test.sh
@@ -3,24 +3,27 @@
 TMP_SUBDOMAIN="tmp-`date +%Y-%m-%d--%H-%M-%S--%N--$RANDOM$RANDOM`"
 TMP_DOMAIN="$TMP_SUBDOMAIN.$DOMAIN"
 
+
+
 go env
 go build -v
 echo "Tmp domain: $TMP_DOMAIN"
 
 echo "Add record"
-./ypdd --ttl 60 $DOMAIN add $TMP_SUBDOMAIN A 127.1.2.3
+./ypdd --ttl 60 $DOMAIN add $TMP_SUBDOMAIN A 127.1.2.3 || exit 1
 
 echo
 echo "Get record list"
 LINE=`./ypdd $DOMAIN list | grep $TMP_SUBDOMAIN`
 echo "$LINE"
+[ -n "$LINE" ] || exit 1
 
 echo
 echo "nslookup"
 LOOKUP=`nslookup $TMP_DOMAIN`
 echo "$LOOKUP"
-echo "$LOOKUP" | grep -q 127.1.2.3
+echo "$LOOKUP" | grep -q 127.1.2.3 || exit 1
 
 echo "Del record"
 ID=`echo "$LINE" | cut -d ' ' -f 1`
-./ypdd $DOMAIN del $ID
+./ypdd $DOMAIN del $ID || exit 1
