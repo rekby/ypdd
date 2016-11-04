d4d8485ccb0539c43fe0bd06bf7825e872bef471
Rekby <timofey@koolin.ru>
2016-11-04 08:57:42 +0300

    add nslookup test

--- travis-test.sh
+++ travis-test.sh
@@ -7,8 +7,20 @@ go env
 go build -v
 echo "Tmp domain: $TMP_DOMAIN"
 
-./ypdd --ttl 60 $DOMAIN add $TMP_SUBDOMAIN A 127.0.0.1
+echo "Add record"
+./ypdd --ttl 60 $DOMAIN add $TMP_SUBDOMAIN A 127.1.2.3
+
+echo
+echo "Get record list"
 LINE=`./ypdd $DOMAIN list | grep $TMP_SUBDOMAIN`
 echo "$LINE"
+
+echo
+echo "nslookup"
+LOOKUP=`nslookup $TMP_DOMAIN`
+echo "$LOOKUP"
+echo "$LOOKUP" | grep -q 127.1.2.3
+
+echo "Del record"
 ID=`echo "$LINE" | cut -d ' ' -f 1`
 ./ypdd $DOMAIN del $ID
